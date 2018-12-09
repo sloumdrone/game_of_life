f37153fefbd938c0ed32213e0b6c408a35e68520
sloumdrone <brian.michael.evans@gmail.com>
2018-12-08 17:26:46 -0800

    Got wall wrapping working and added q key to quit

--- gol.py
+++ gol.py
@@ -2,28 +2,68 @@
 from random import randint
 from time import sleep
 import curses
-
+import sys
 
 
 def check_surroundings(x, y, arr, h, w):
     c = '\u2588'
     neighbors = 0
-    if y - 1 >= 0 and arr[y-1][x] == c:
+    if y - 1 >= 0 and arr[y - 1][x] == c:
         neighbors += 1
-    if y + 1 < h and arr[y+1][x] == c:
+    elif y - 1 < 0 and arr[h - 1][x]:
         neighbors += 1
-    if x - 1 >= 0 and arr[y][x-1] == c:
+
+    if y + 1 < h and arr[y + 1][x] == c:
         neighbors += 1
-    if x + 1 < w and arr[y][x+1] == c:
+    elif y + 1 >= h and arr[0][x]:
         neighbors += 1
-    if x - 1 >= 0 and y - 1 >= 0 and arr[y-1][x-1] == c:
+
+    if x - 1 >= 0 and arr[y][x - 1] == c:
         neighbors += 1
-    if x + 1 < w and y + 1 < h and arr[y+1][x+1] == c:
+    elif x - 1 < 0 and arr[y][w - 1]:
         neighbors += 1
-    if x + 1 < w and y - 1 >= 0 and arr[y-1][x+1] == c:
+
+    if x + 1 < w and arr[y][x + 1] == c:
         neighbors += 1
-    if x - 1 >= 0 and y + 1 < h and arr[y+1][x-1] == c:
+    elif x + 1 >= w and arr[y][0]:
         neighbors += 1
+
+    if x - 1 >= 0 and y - 1 >= 0 and arr[y - 1][x - 1] == c:
+        neighbors += 1
+    elif x - 1 < 0 and y - 1 >= 0 and arr[y - 1][w - 1]:
+        neighbors += 1
+    elif x - 1 >= 0 and y - 1 < 0 and arr[h - 1][x - 1]:
+        neighbors += 1
+    elif x - 1 < 0 and y - 1 < 0 and arr[h - 1][w - 1]:
+        neighbors += 1
+
+    if x + 1 < w and y + 1 < h and arr[y + 1][x + 1] == c:
+        neighbors += 1
+    elif x + 1 >= w and y + 1 < h and arr[y + 1][0] == c:
+        neighbors += 1
+    elif x + 1 < w and y + 1 >= h and arr[0][x + 1] == c:
+        neighbors += 1
+    elif x + 1 >= w and y + 1 >= h and arr[0][0]:
+        neighbors += 1
+
+    if x + 1 < w and y - 1 >= 0 and arr[y - 1][x + 1] == c:
+        neighbors += 1
+    elif x + 1 >= w and y - 1 >= 0 and arr[y - 1][0] == c:
+        neighbors += 1
+    elif x + 1 < w and y - 1 < 0 and arr[h - 1][x + 1] == c:
+        neighbors += 1
+    elif x + 1 >= w and y - 1 < 0 and arr[h - 1][0] == c:
+        neighbors += 1
+
+    if x - 1 >= 0 and y + 1 < h and arr[y + 1][x - 1] == c:
+        neighbors += 1
+    elif x - 1 < 0 and y + 1 >= h and arr[0][w - 1] == c:
+        neighbors += 1
+    elif x - 1 >= 0 and y + 1 >= h and arr[0][x - 1] == c:
+        neighbors += 1
+    elif x - 1 < 0 and y + 1 < h and arr[y + 1][w - 1] == c:
+        neighbors += 1
+
     return neighbors
 
 
@@ -42,7 +82,7 @@ def update_cell(x, y, arr, h, w):
 def random_start(arr,h,w):
     for y in range(h):
         for x in range(w):
-            arr[y][x] = '\u2588' if randint(1,10) == 5 else ' '
+            arr[y][x] = '\u2588' if randint(1,6) == 5 else ' '
 
 
 def main(screen):
@@ -61,9 +101,22 @@ def main(screen):
     # char to print for a live cell
     c = '\u2588'
 
+    #seed the first round
     random_start(s1,h,w)
+    for y in range(h):
+        for x in range(w):
+            s2[y][x] = update_cell(x, y, s1, h, w)
+    for y in range(h):
+        for x in range(w):
+            s1[y][x] = s2[y][x]
+
 
     while True:
+        inp = screen.getch()
+        curses.flushinp()
+        if inp == 113:
+            sys.exit(0)
+
         screen.erase()
         for y in range(h):
             for x in range(w):
